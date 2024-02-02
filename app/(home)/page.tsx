3b2c5baa38e4cc8fcba96e8fdc2ac9f292c7d6d2
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-02-01 23:33:05 -0300

    feat: add footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -44,7 +44,7 @@ export default async function Home() {
           ))}
         </div>
 
-        <div className="mt-6">
+        <div className="mt-6 mb-[4.5rem]" >
         <h2 className="px-5 text-xs uppercase text-gray-400 font-bold mb-3">
           Populares
         </h2>
